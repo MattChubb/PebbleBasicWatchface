72e13b07091ac1a1af20f9d12de5ad1465a9e6af
Matthew Chubb <chubb.matthew@gmail.com>
2015-09-12 14:17:03 +0100

    Use custom font perfect_dos

--- src/main.c
+++ src/main.c
@@ -2,17 +2,18 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 
 static void update_time() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-  static char buffer[] = "00:00";
+  static char buffer[] = "0000";
   
   if (clock_is_24h_style() == true) {
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(buffer, sizeof("0000"), "%H%M", tick_time);
   } else {
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(buffer, sizeof("0000"), "%I%M", tick_time);
   }
   
   text_layer_set_text(s_time_layer, buffer);
@@ -25,11 +26,12 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 
 static void main_window_load(Window *window) {
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_time_layer = text_layer_create(GRect(0, 50, 144, 55));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_VGA_52));
   
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
@@ -55,6 +57,7 @@ static void init() {
 
 static void deinit() {
   window_destroy(s_main_window);
+  fonts_unload_custom_font(s_time_font);
 }
 
 
